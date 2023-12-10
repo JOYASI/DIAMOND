1b85e287ee1f1226e6348dd777381a7b2aec4438
JOYASI <PROJECT@joels-air.home>
2023-12-10 23:38:33 +0000

    completed the UI for the register and the welcome views. also gave buttons some actions to navigate between screens

--- Project-Diamond/Project-Diamond/Controllers/WelcomeViewController.swift
+++ Project-Diamond/Project-Diamond/Controllers/WelcomeViewController.swift
@@ -11,19 +11,18 @@ class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        if sender.titleLabel?.text == "Go to Login" {
+            self.performSegue(withIdentifier: "goToLogin", sender: self)
+        }
+        else {
+            self.performSegue(withIdentifier: "goToRegister", sender: self)
+        }
+        
     }
-    */
-
 }
